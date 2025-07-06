fc2b0349ac6b43c5f763eaeb304b74f7a28f0c64
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-07-06 23:47:47 +0200

    Add ui test for generics on `loop.cycle`

--- testing/tests/ui/loop_cycle_empty.rs
+++ testing/tests/ui/loop_cycle_empty.rs
@@ -7,5 +7,12 @@ use askama::Template;
 )]
 struct ForCycleEmpty;
 
+#[derive(Template)]
+#[template(
+    source = r#"{% for v in values %}{{ loop.cycle::<u8>([]) }}{{ v }},{% endfor %}"#,
+    ext = "txt"
+)]
+struct ForCycleGenerics;
+
 fn main() {
 }
