6035ffb013e5cfa93047f6e0ce0e23c83407624b
René Kijewski <rene.kijewski@fu-berlin.de>
2025-07-27 04:29:27 +0200

    derive: track included files with relative path
    To make the compiler understand which files contributed to the generated
    code, in order to make it aware when a recompilation is needed, we track
    the file paths using `include_bytes!()` calls in the generated code.
    This can be a problem for reproducible builds, because the working
    directory contributes to the file hash.
    
    This PR makes Askama generate relative paths if possible. This is always
    possible on Unix systems, but on Windows hosts absolute paths have to be
    used if the code is kept on different drives.

--- askama_derive/src/generator/helpers/paths.rs
+++ askama_derive/src/generator/helpers/paths.rs
@@ -23,12 +23,9 @@ use std::path::{Component, Path, PathBuf};
 /// 5. Leave intact `..` elements that begin a non-rooted path.
 ///
 /// If the result of this process is an empty string, return the string `"."`, representing the current directory.
-pub(crate) fn clean<P>(path: P) -> PathBuf
-where
-    P: AsRef<Path>,
-{
+pub(crate) fn clean(path: &Path) -> PathBuf {
     let mut out = Vec::new();
-    for comp in path.as_ref().components() {
+    for comp in path.components() {
         match comp {
             Component::CurDir => (),
             Component::ParentDir => match out.last() {
@@ -52,38 +49,7 @@ where
 }
 
 /// Construct a relative path from a provided base directory path to the provided path.
-///
-/// ```rust
-/// use pathdiff::diff_paths;
-/// use std::path::*;
-///
-/// assert_eq!(diff_paths("/foo/bar",      "/foo/bar/baz"),  Some("../".into()));
-/// assert_eq!(diff_paths("/foo/bar/baz",  "/foo/bar"),      Some("baz".into()));
-/// assert_eq!(diff_paths("/foo/bar/quux", "/foo/bar/baz"),  Some("../quux".into()));
-/// assert_eq!(diff_paths("/foo/bar/baz",  "/foo/bar/quux"), Some("../baz".into()));
-/// assert_eq!(diff_paths("/foo/bar",      "/foo/bar/quux"), Some("../".into()));
-///
-/// assert_eq!(diff_paths("/foo/bar",      "baz"),           Some("/foo/bar".into()));
-/// assert_eq!(diff_paths("/foo/bar",      "/baz"),          Some("../foo/bar".into()));
-/// assert_eq!(diff_paths("foo",           "bar"),           Some("../foo".into()));
-///
-/// assert_eq!(
-///     diff_paths(&"/foo/bar/baz", "/foo/bar".to_string()),
-///     Some("baz".into())
-/// );
-/// assert_eq!(
-///     diff_paths(Path::new("/foo/bar/baz"), Path::new("/foo/bar").to_path_buf()),
-///     Some("baz".into())
-/// );
-/// ```
-pub(crate) fn diff_paths<P, B>(path: P, base: B) -> Option<PathBuf>
-where
-    P: AsRef<Path>,
-    B: AsRef<Path>,
-{
-    let path = path.as_ref();
-    let base = base.as_ref();
-
+pub(crate) fn diff_paths(path: &Path, base: &Path) -> Option<PathBuf> {
     if path.is_absolute() != base.is_absolute() {
         if path.is_absolute() {
             Some(PathBuf::from(path))
@@ -104,8 +70,8 @@ where
                 }
                 (None, _) => comps.push(Component::ParentDir),
                 (Some(a), Some(b)) if comps.is_empty() && a == b => (),
-                (Some(a), Some(b)) if b == Component::CurDir => comps.push(a),
-                (Some(_), Some(b)) if b == Component::ParentDir => return None,
+                (Some(a), Some(Component::CurDir)) => comps.push(a),
+                (Some(_), Some(Component::ParentDir)) => return None,
                 (Some(a), Some(_)) => {
                     comps.push(Component::ParentDir);
                     for _ in itb {
@@ -119,4 +85,41 @@ where
         }
         Some(comps.iter().map(|c| c.as_os_str()).collect())
     }
+}
+
+#[test]
+fn test_diff_paths() {
+    assert_eq!(
+        diff_paths(Path::new("/foo/bar"), Path::new("/foo/bar/baz")),
+        Some("../".into())
+    );
+    assert_eq!(
+        diff_paths(Path::new("/foo/bar/baz"), Path::new("/foo/bar")),
+        Some("baz".into())
+    );
+    assert_eq!(
+        diff_paths(Path::new("/foo/bar/quux"), Path::new("/foo/bar/baz")),
+        Some("../quux".into())
+    );
+    assert_eq!(
+        diff_paths(Path::new("/foo/bar/baz"), Path::new("/foo/bar/quux")),
+        Some("../baz".into())
+    );
+    assert_eq!(
+        diff_paths(Path::new("/foo/bar"), Path::new("/foo/bar/quux")),
+        Some("../".into())
+    );
+
+    assert_eq!(
+        diff_paths(Path::new("/foo/bar"), Path::new("baz")),
+        Some("/foo/bar".into())
+    );
+    assert_eq!(
+        diff_paths(Path::new("/foo/bar"), Path::new("/baz")),
+        Some("../foo/bar".into())
+    );
+    assert_eq!(
+        diff_paths(Path::new("foo"), Path::new("bar")),
+        Some("../foo".into())
+    );
 }
